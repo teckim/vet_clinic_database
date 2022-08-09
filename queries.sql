4d2589e1fffa2e8a6ee5246fa2d76315e9b5e850
BHDHakim <hakim.bhd@gmail.com>
2022-08-09 13:20:41 +0300

    Add species

--- queries.sql
+++ queries.sql
@@ -1,10 +1,112 @@
 /*Queries that provide answers to the questions from all projects.*/
+SELECT
+  *
+FROM
+  animals
+WHERE
+  name LIKE '%mon';
 
-SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
-SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
-SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
-SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
-SELECT * FROM animals WHERE neutered = TRUE;
-SELECT * FROM animals WHERE name <> 'Gabumon';
-SELECT * FROM animals WHERE round(weight_kg::numeric, 2) >= 10.23 AND round(weight_kg::numeric, 2) <= 17.3;
+SELECT
+  name
+FROM
+  animals
+WHERE
+  date_of_birth BETWEEN '2016-01-01'
+  AND '2019-12-31';
+
+SELECT
+  name
+FROM
+  animals
+WHERE
+  neutered = TRUE
+  AND escape_attempts < 3;
+
+SELECT
+  date_of_birth
+FROM
+  animals
+WHERE
+  name IN ('Agumon', 'Pikachu');
+
+SELECT
+  name,
+  escape_attempts
+FROM
+  animals
+WHERE
+  weight_kg > 10.5;
+
+SELECT
+  *
+FROM
+  animals
+WHERE
+  neutered = TRUE;
+
+SELECT
+  *
+FROM
+  animals
+WHERE
+  name <> 'Gabumon';
+
+SELECT
+  *
+FROM
+  animals
+WHERE
+  round(weight_kg :: numeric, 2) >= 10.23
+  AND round(weight_kg :: numeric, 2) <= 17.3;
+
+-- AGGREGATIONS AND GROUP BY
+-- How many animals are there?
+SELECT
+  COUNT(*)
+FROM
+  animals;
+
+-- How many animals have never tried to escape?
+SELECT
+  COUNT(*) AS "No: animals never escaped"
+FROM
+  animals
+WHERE
+  escape_attempts = 0;
+
+-- What is the average weight of animals?
+SELECT
+  AVG(weight_kg) AS "average weight"
+FROM
+  animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+SELECT
+  neutered,
+  COUNT(*)
+FROM
+  animals
+GROUP BY
+  neutered;
+
+-- What is the minimum and maximum weight of each type of animal?
+SELECT
+  species,
+  MIN(weight_kg) AS minimum,
+  MAX(weight_kg) AS maximum
+FROM
+  animals
+GROUP BY
+  species;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT
+  species,
+  ROUND(AVG(escape_attempts), 0) AS "average number of escape attempts"
+FROM
+  animals
+WHERE
+  date_of_birth BETWEEN '1990-01-01'
+  AND '2000-12-31'
+GROUP BY
+  species;
