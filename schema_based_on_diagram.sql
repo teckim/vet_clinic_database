2477932af86d0dbe8f2327ab028c17803c33696e
Terbeche Mustapha <termus96@gmail.com>
2022-08-16 14:15:00 +0100

    Add treatments, invoice_items and treatments_medical_histories tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -21,3 +21,26 @@ CREATE TABLE invoices(
   medical_history__id INT REFERENCES medical_histories(id),
   PRIMARY KEY(id)
 );
+
+CREATE TABLE treatments(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  type VARCHAR(20),
+  name VARCHAR(20),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE invoice_items(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  unit_price DECIMAL,
+  quantity INT,
+  total_price DECIMAL,
+  invoice_id INT REFERENCES invoices(id),
+  treatment_id INT REFERENCES treatments(id),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE treatments_medical_histories(
+  treatment_id INT REFERENCES treatments(id),
+  medical_history_id INT REFERENCES medical_histories(id),
+  PRIMARY KEY(treatment_id, medical_history_id)
+);
