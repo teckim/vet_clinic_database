5414dbcb62dd51d94430968bad7a6d94b6061e04
BHDHakim <hakim.bhd@gmail.com>
2022-08-16 16:07:17 +0300

    Add database diagram

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,21 @@
+CREATE TABLE medical_histories(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  admitted_at TIMESTAMP,
+  patient_id INT REFERENCES patients(id),
+  status VARCHAR(20),
+  PRIMARY KEY(id)
+);
+CREATE TABLE patients(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(20),
+  date_of_birth DATE,
+  PRIMARY KEY(id)
+);
+CREATE TABLE invoices(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  total_amount DECIMAL,
+  generated_at TIMESTAMP,
+  payed_at TIMESTAMP,
+  medical_history__id INT REFERENCES medical_histories(id),
+  PRIMARY KEY(id)
+);
